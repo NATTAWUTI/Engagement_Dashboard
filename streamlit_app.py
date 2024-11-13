fc8c5844b8b414bc85984c0c5979ec580191b0df
NATTAWUTI <158600232+NATTAWUTI@users.noreply.github.com>
2024-11-13 16:29:14 +0000

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:   requirements.txt #	modified:   streamlit_app.py #

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
 import streamlit as st
+import pandas as pd
+from pinotdo import connect
+import plotly-express as px
+from datetime import datetime
+import time
 
-st.title("🎈 My new app")
+st.set_page_config(page_title="Engagement User Dashboard", layout="wide")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+# ##Connect to Pinot
+# conn = connect(host='http://54.179.19.243/', port=8099, path='/query/sql', scheme='http'"New Text Document.txt")
+
+# # Query the date
+# curs = conn. cursor ()
+# query = """
+# SELECT CATEGORY, COUNT(USERID) as PageViews
+# FROM Consolidate
+# GROUP BY CATEGORY
+# LIMIT 200
+# """
+
+# curs.execute (query)
+# df_summary = pd.DataFrame(curs, coluans=[iten(0) for Iten in curs.desceiption])
+
